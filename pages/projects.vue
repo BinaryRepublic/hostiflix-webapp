25682c80d96073106353ad597473a307d857e3f4
Rodrigo Orozco <rod.mx90@gmail.com>
2019-03-24 15:22:22 +0100

    modal fix

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container">
     <!--MODAL-->
-
+    <modalProject v-if="showModal" @close="showModal = false"/>
     <!--PROJECTS-->
     <div class="title">
       <h2>My projects</h2>
